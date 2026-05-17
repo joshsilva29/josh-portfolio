c22309450b1cf6debbae2e01892727279eb18d89
joshsilva29 <106221807+joshsilva29@users.noreply.github.com>
2026-05-17 00:44:49 -0400

    finished contact page

--- src/pages/contact-page/ContactPage.tsx
+++ src/pages/contact-page/ContactPage.tsx
@@ -1,9 +1,22 @@
-import NavBar from "../../components/nav-bar/NavBar"
+import NavBar from "../../components/nav-bar/NavBar";
+import "./contactpage.css";
 
 function ContactPage() {
     return (
         <div>
             <NavBar />
+            <div className="container">
+                <div className="bold" id="contact-text">reach me at: </div>
+                <div>
+                    <a href="mailto:joshuasilva1117@gmail.com">joshuasilva1117@gmail.com</a>
+                </div>
+                <div>
+                    <a href="https://www.linkedin.com/in/joshua-silva-359756177/">linkedin</a>
+                </div>
+                <div>
+                    <a href="https://github.com/joshsilva29">github</a>
+                </div>
+            </div>
         </div>
     );
 }
